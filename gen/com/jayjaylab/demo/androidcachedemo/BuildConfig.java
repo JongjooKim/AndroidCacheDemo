03305d43261055e9ac40c07da4db8caf1f607d94
Jongjoo Kim <powerqn@gmail.com>
2014-05-18 20:30:00 +0900

    removed bin gen directories

--- gen/com/jayjaylab/demo/androidcachedemo/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.jayjaylab.demo.androidcachedemo;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
